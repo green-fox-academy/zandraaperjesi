3e0928ba3069cabaaa7fe06fb523b9469d4aa2b5
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-21 16:37:28 +0100

    add exercise 28 final

--- week-02/day-02/exercise-01/src/DrawPyramid2.java
+++ week-02/day-02/exercise-01/src/DrawPyramid2.java
@@ -11,13 +11,13 @@ public class DrawPyramid2 {
 
     for (int i = 1; i < n + 1; i += 2) {
       for (int j = 0; j < ((n - i) /2) + 1; j++) {
-        System.out.print("g");
+        System.out.print(" ");
       }
       for (int j = 0; j < i; j++) {
         System.out.print("*");
       }
       for (int j = 0; j < ((n - i) /2) + 1; j++) {
-        System.out.print("g");
+        System.out.print(" ");
       }
       System.out.println("");
     }
