9d7970d02b8c3eb6dc22d3f73afe6f9bada84f24
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-21 17:33:35 +0100

    add random number to guessing game

--- week-02/day-02/exercise-01/src/GuessTheNumber.java
+++ week-02/day-02/exercise-01/src/GuessTheNumber.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Created by zandraa on 3/21/2017.
@@ -7,10 +8,12 @@ public class GuessTheNumber {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     boolean isGuessed = false;
-    int numberToGuess = 8;
+
+    Random random = new Random();
+    int numberToGuess = random.nextInt(50) + 1;
 
     while (isGuessed == false) {
-      System.out.println("Guess a number: ");
+      System.out.println("Guess a number between 1 and 50: ");
       int n = myScanner.nextInt();
 
       if (n > numberToGuess) {
@@ -20,7 +23,7 @@ public class GuessTheNumber {
         System.out.println("The stored number is higher");
       }
       else {
-        System.out.println("You found the number: 8");
+        System.out.println("You found the number: " + numberToGuess);
         isGuessed = true;
       }
     }
