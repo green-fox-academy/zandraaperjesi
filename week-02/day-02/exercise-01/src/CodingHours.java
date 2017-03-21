5dce76a096333268e6c46cb94fa3128c451fb44c
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-21 14:00:44 +0100

    add exercise 6 7

--- week-02/day-02/exercise-01/src/CodingHours.java
+++ week-02/day-02/exercise-01/src/CodingHours.java
@@ -2,4 +2,10 @@
  * Created by zandraa on 3/21/2017.
  */
 public class CodingHours {
+  public static void main(String[] args) {
+    int hoursSpentCoding = 17 * 5 * 6;
+    System.out.println("hours spent coding: " + hoursSpentCoding);
+    int percentageCodingPerSemester = hoursSpentCoding * 100 / (17 * 52);
+    System.out.println("the percentage of the coding hours in the semester: " + percentageCodingPerSemester);
+  }
 }
---
+++ week-02/day-02/exercise-01/src/FavoriteNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zandraa on 3/21/2017.
+ */
+public class FavoriteNumber {
+  public static void main(String[] args) {
+    int favoriteNumber = 8;
+    System.out.println("My favorite number is: " + favoriteNumber);
+  }
+}
