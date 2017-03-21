420d0d2d3a82ecd805c05d0be3fb3244ca038f14
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-21 16:36:44 +0100

    add exercise 28 with placeholders for whitespace

---
+++ week-02/day-02/exercise-01/src/DrawPyramid2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by zandraa on 3/21/2017.
+ */
+public class DrawPyramid2 {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Give me a number: ");
+    int n = myScanner.nextInt();
+
+    for (int i = 1; i < n + 1; i += 2) {
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print("g");
+      }
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print("g");
+      }
+      System.out.println("");
+    }
+  }
+}
