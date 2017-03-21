46fc03222f236cffb8ae05e2f60499e9cd2da2e6
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-21 16:42:14 +0100

    add exercise 29

---
+++ week-02/day-02/exercise-01/src/DrawDiamond.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * Created by zandraa on 3/21/2017.
+ */
+public class DrawDiamond {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Give me a number: ");
+    int n = myScanner.nextInt();
+
+    for (int i = 1; i < n + 1; i += 2) {
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print(" ");
+      }
+      System.out.println("");
+    }
+
+    for (int i = n - 2; i > 0; i -= 2) {
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      for (int j = 0; j < ((n - i) /2) + 1; j++) {
+        System.out.print(" ");
+      }
+      System.out.println("");
+    }
+  }
+}
+
