4727e6892a25b5cab93227d43a42d0fa64ffc2ba
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 14:58:44 +0100

    Cuboid asks for user input, now works with integers

--- week-02/day-02/exercise-01/src/Cuboid.java
+++ week-02/day-02/exercise-01/src/Cuboid.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by zandraa on 3/21/2017.
  */
 public class Cuboid {
   public static void main(String[] args) {
-    double x = 12.34;
-    double y = 15.32;
-    double z = 18.77;
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Cube's width: ");
+    int x = myScanner.nextInt();
+    System.out.println("Cube's other width: ");
+    int y = myScanner.nextInt();
+    System.out.println("Cube's height: ");
+    int z = myScanner.nextInt();
 
-    int surfaceArea = (int) (x * y * 2 + z * x * 2 + z * y * 2);
-    int volume = (int) (y * x * z);
+    int surfaceArea = (x * y * 2 + z * x * 2 + z * y * 2);
+    int volume = (y * x * z);
     System.out.println("Surface Area: " + surfaceArea);
     System.out.println("Volume: " + volume);
   }
