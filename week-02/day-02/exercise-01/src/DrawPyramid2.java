b7cf3840d87c90578cae47e1b6d4c6e580b6d29b
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 14:17:21 +0100

    fix corresponding pyramid height for user input

--- week-02/day-02/exercise-01/src/DrawPyramid2.java
+++ week-02/day-02/exercise-01/src/DrawPyramid2.java
@@ -7,7 +7,8 @@ public class DrawPyramid2 {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     System.out.println("Give me a number: ");
-    int n = myScanner.nextInt();
+    int numGiven = myScanner.nextInt();
+    int n = numGiven * 2;
 
     for (int i = 1; i < n + 1; i += 2) {
       for (int j = 0; j < ((n - i) /2) + 1; j++) {
@@ -15,9 +16,6 @@ public class DrawPyramid2 {
       }
       for (int j = 0; j < i; j++) {
         System.out.print("*");
-      }
-      for (int j = 0; j < ((n - i) /2) + 1; j++) {
-        System.out.print(" ");
       }
       System.out.println("");
     }
