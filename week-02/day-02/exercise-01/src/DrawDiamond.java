6fec68f18c38a2bf51ad70a24e742f98a42c3687
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 14:09:32 +0100

    add check for user input in DrawDiamond to make sure its an odd number

--- week-02/day-02/exercise-01/src/DrawDiamond.java
+++ week-02/day-02/exercise-01/src/DrawDiamond.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class DrawDiamond {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
-    System.out.println("Give me a number: ");
-    int n = myScanner.nextInt();
+    int n = 0;
+
+    do {
+      System.out.println("Give me an odd number: ");
+      n = myScanner.nextInt();
+    }  while((n % 2) != 1);
 
     for (int i = 1; i < n + 1; i += 2) {
       for (int j = 0; j < ((n - i) /2) + 1; j++) {
