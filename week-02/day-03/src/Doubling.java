266cde8a57ef17c2b21691537e0d2d23137675db
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 11:16:09 +0100

    add Doubling with private function

--- week-02/day-03/src/Doubling.java
+++ week-02/day-03/src/Doubling.java
@@ -2,4 +2,17 @@
  * Created by zandraa on 3/22/2017.
  */
 public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    System.out.println(doubling(ak));
+  }
+
+  private static int doubling(int toDouble) {
+    return 2 * toDouble;
+  }
+
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
+
 }
