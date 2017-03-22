e911dac908901ec82e05e3bb2193117dd4631459
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 12:58:24 +0100

    add PrintAll

---
+++ week-02/day-03/src/PrintAll.java
@@ -0,0 +1,17 @@
+
+/**
+ * Created by zandraa on 3/22/2017.
+ */
+public class PrintAll {
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+
+    for (int i = 0; i < af.length; i++) {
+      System.out.println(af[i]);
+    }
+  }
+}
+
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
