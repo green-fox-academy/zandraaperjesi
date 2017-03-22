62ba60469381171484bd9966aae5a7a16f612c31
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 12:27:49 +0100

    add Third

---
+++ week-02/day-03/src/Third.java
@@ -0,0 +1,15 @@
+/**
+ * Created by zandraa on 3/22/2017.
+ */
+public class Third {
+  public static void main(String[] args) {
+
+    int[] q = {4, 5, 6, 7};
+    System.out.println(q[2]);
+
+  }
+}
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
