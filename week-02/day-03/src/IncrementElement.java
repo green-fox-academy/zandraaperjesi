e7aef63fea5b747c90c3b690613dd75a569e0e07
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 12:53:28 +0100

    add Increment Element

---
+++ week-02/day-03/src/IncrementElement.java
@@ -0,0 +1,17 @@
+/**
+ * Created by zandraa on 3/22/2017.
+ */
+public class IncrementElement {
+  public static void main(String[] args) {
+    int[] t = {1, 2, 3, 4, 5};
+
+    t[2] += 1;
+
+    System.out.println(t[2]);
+  }
+}
+
+//- Create an array variable named `t`
+//with the following content: `[1, 2, 3, 4, 5]`
+//- Increment the third element
+//- Print the third element
