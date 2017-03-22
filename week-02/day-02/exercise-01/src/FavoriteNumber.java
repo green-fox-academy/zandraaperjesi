0e369a5ce3caa9401d0f71bd77413fe68c04c118
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-22 14:44:50 +0100

    change favourite number from hardcoded to user input

--- week-02/day-02/exercise-01/src/FavoriteNumber.java
+++ week-02/day-02/exercise-01/src/FavoriteNumber.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by zandraa on 3/21/2017.
  */
 public class FavoriteNumber {
   public static void main(String[] args) {
-    int favoriteNumber = 8;
-    System.out.println("My favorite number is: " + favoriteNumber);
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("What is your favorite number? ");
+    int favoriteNumber = myScanner.nextInt();
+
+    System.out.println("Your favorite number is: " + favoriteNumber);
   }
 }
