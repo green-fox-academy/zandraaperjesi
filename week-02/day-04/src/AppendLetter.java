fe314a8932e1bd9a8cf00c179d65b7c7a0cc6706
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 13:56:46 +0100

    add AppendLetter that concats a letter a to the end of the words inside the list laos update gitignore file

---
+++ week-02/day-04/src/AppendLetter.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by zandraa on 3/23/2017.
+ */
+public class AppendLetter {
+  public static void main(String... args) {
+    ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+    for (int i = 0; i < far.size(); i++) {
+      far.set(i, far.get(i).concat("a"));
+    }
+
+    System.out.println(far);
+  }  
+}
+
+// Add "a" to every string in the far list.
