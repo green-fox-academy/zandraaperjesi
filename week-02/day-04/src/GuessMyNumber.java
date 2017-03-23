2919d2d4332bc1064a4df465f55598fc43950841
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 17:50:31 +0100

    fix bug when guessing correctly on 1 life player now wins

--- week-02/day-04/src/GuessMyNumber.java
+++ week-02/day-04/src/GuessMyNumber.java
@@ -8,10 +8,17 @@ public class GuessMyNumber {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     Random rand = new Random();
-    int  numToGuess = rand.nextInt(100) + 1;
 
-    System.out.println("I've the number between 1-100. You have 5 lives.");
-
+    System.out.println("I will think of a random number and you will guess.");
+    System.out.println("What is the highest number I can think of?");
+    while (!myScanner.hasNextInt()) {
+        myScanner.next();
+        System.out.println("Please give me a number!");
+    }
+    int numberCap = myScanner.nextInt();
+    System.out.println("I've the number between 1-" + numberCap + ". You have 5 lives.");
+    int  numToGuess = rand.nextInt(numberCap) + 1;
+    System.out.println(numToGuess);
     for (int i = 4; i >= 0; i--) {
       System.out.println("Your guess: ");
       while (!myScanner.hasNextInt()) {
@@ -20,18 +27,18 @@ public class GuessMyNumber {
       }
 
       int guess = myScanner.nextInt();
-      if (i == 0) {
-        System.out.println("The number was: " + numToGuess + ". Better luck next time!");
+      if (guess == numToGuess) {
+        System.out.println("Congratulations. You won! :)");
+        break;
+      }
+      else if (i == 0) {
+        System.out.println("The number was: " + numToGuess + " :( . Better luck next time!");
       }
       else if (guess < numToGuess) {
         System.out.println("Too low. You have " + i + " lives left.");
       }
       else if (guess > numToGuess) {
         System.out.println("Too high. You have " + i + " lives left.");
-      }
-      else if (guess == numToGuess) {
-        System.out.println("Congratulations. You won!");
-        break;
       }
     }
   }
