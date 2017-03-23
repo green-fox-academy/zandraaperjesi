da8d5e7e5d0b8f0d0bd89914254b0307cc812311
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 12:41:21 +0100

    modify TakesLonger to use Stringbuilder insted of the .concat on .concat solution

--- week-02/day-04/src/TakesLonger.java
+++ week-02/day-04/src/TakesLonger.java
@@ -4,7 +4,11 @@
 public class TakesLonger {
   public static void main(String... args){
     String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
-    quote = quote.substring(0, quote.indexOf("you")).concat("always takes longer than ").concat(quote.substring(quote.indexOf("you"), quote.length()));
+
+    StringBuilder correctQuote = new StringBuilder();
+    correctQuote.append(quote);
+    correctQuote.insert(quote.indexOf("you"), "always takes longer than ");
+    quote = correctQuote.toString();
     System.out.println(quote);
   }
 }
