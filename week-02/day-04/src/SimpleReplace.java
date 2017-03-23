15628d70732604daf07a06971247ef81ab285a8d
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 11:41:17 +0100

    add simple replace, fix code with with assigning old string to the correct one

--- week-02/day-04/src/SimpleReplace.java
+++ week-02/day-04/src/SimpleReplace.java
@@ -2,4 +2,16 @@
  * Created by zandraa on 3/23/2017.
  */
 public class SimpleReplace {
+  public static void main(String... args){
+    String example = "In a dishwasher far far away";
+
+    example = example.replace("dishwasher", "galaxy");
+
+    System.out.println(example);
+  }
+
 }
+
+// I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
