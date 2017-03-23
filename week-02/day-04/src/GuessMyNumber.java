34dccd1807b75cc28bae5fed59b67cf2b6a160b3
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 17:09:45 +0100

    add game loop to GuessMyNumber

--- week-02/day-04/src/GuessMyNumber.java
+++ week-02/day-04/src/GuessMyNumber.java
@@ -9,11 +9,25 @@ public class GuessMyNumber {
     Scanner myScanner = new Scanner(System.in);
     Random rand = new Random();
     int  numToGuess = rand.nextInt(100) + 1;
-
+    System.out.println(numToGuess);
     System.out.println("I've the number between 1-100. You have 5 lives.");
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 4; i >= 0; i--) {
       System.out.println("Your guess: ");
+      int guess = myScanner.nextInt();
+      if (i == 0) {
+        System.out.println("The number was: " + numToGuess + "Better luck next time!");
+      }
+      else if (guess < numToGuess) {
+        System.out.println("Too low. You have " + i + " lives left.");
+      }
+      else if (guess > numToGuess) {
+        System.out.println("Too high. You have " + i + " lives left.");
+      }
+      else if (guess == numToGuess) {
+        System.out.println("Congratulations. You won!");
+        break;
+      }
     }
   }
 }
