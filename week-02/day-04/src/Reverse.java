bd67033580375cdcdb84373ada583272f6d0e266
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-23 12:13:13 +0100

    add reverse with reverser function with adding chars to an empty string

---
+++ week-02/day-04/src/Reverse.java
@@ -0,0 +1,22 @@
+/**
+ * Created by zandraa on 3/23/2017.
+ */
+public class Reverse {
+  public static void main(String... args){
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+    reverser(reversed);
+    System.out.println(reversed);
+  }
+
+  private static void reverser(String toReverse) {
+    String correctOrder = "";
+    for (int i = 0; i < toReverse.length(); i++) {
+      correctOrder += (toReverse.charAt(toReverse.length() - 1 - i));
+    }
+    System.out.println(correctOrder);
+  }
+}
+
+// Create a function that can reverse a String, which is passed as the parameter
+// Use it on this reversed string to check it!
+// Try to solve this using charAt() first, and optionally anything else after.
