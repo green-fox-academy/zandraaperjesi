50c98f653337d0007c3551fdbcd8039db38c8f55
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-24 09:01:11 +0100

    add nested for loops in PalindromeSearcher to find same letters

--- week-02/day-05/src/PalindromeSearcher.java
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -7,5 +7,17 @@ public class PalindromeSearcher {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     System.out.println("Give me a line and I will show you the palindromes in it.");
+
+    String inputString = myScanner.nextLine();
+
+    char[] slicedUpInput = inputString.toCharArray();
+
+    for (int i = 2; i < slicedUpInput.length; i++) {
+      for (int j = 0; j < slicedUpInput.length - i; j++) {
+        if (slicedUpInput[i] == slicedUpInput[j]) {
+          System.out.println("found the same letters");
+        }
+      }
+    }
   }
 }
