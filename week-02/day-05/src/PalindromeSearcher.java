b1febd8fd5777e3bba3804b28025fdd84e1bff13
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-24 08:48:50 +0100

    add PalindromeSearcher

---
+++ week-02/day-05/src/PalindromeSearcher.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * Created by zandraa on 3/24/2017.
+ */
+public class PalindromeSearcher {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Give me a line and I will show you the palindromes in it.");
+  }
+}
