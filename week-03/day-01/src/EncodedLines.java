9f81ca06e0f47b683a6b7d7b64fecd4a2330e2c8
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-27 14:28:41 +0200

    add if whitespace to handle wthitespace input correctly for later output to txt

--- week-03/day-01/src/EncodedLines.java
+++ week-03/day-01/src/EncodedLines.java
@@ -13,16 +13,26 @@ public class EncodedLines {
     List<String> encodedLines = new ArrayList<>();
     List<String> decodedLines = new ArrayList<>();
     Path inputPath = Paths.get("assets/encoded-lines.txt");
+    Path outputPath = Paths.get("assets/decoded-lines.txt");
     try {
       encodedLines = Files.readAllLines(inputPath);
       for (String line : encodedLines) {
         StringBuffer decoder = new StringBuffer();
         for (int i = 0; i < line.length(); i++) {
           int charValue = line.charAt(i);
-          decoder.append(String.valueOf((char) (charValue - 1)));
+          if (!Character.isWhitespace(line.charAt(i))) {
+            decoder.append(String.valueOf((char) (charValue - 1)));
+          }
+          else decoder.append(line.charAt(i));
         }
         System.out.println(decoder);
+        decodedLines.add(decoder.toString());
       }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      Files.write(outputPath, decodedLines);
     } catch (IOException e) {
       e.printStackTrace();
     }
