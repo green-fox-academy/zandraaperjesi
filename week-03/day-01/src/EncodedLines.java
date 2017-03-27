74e7d042a6dad8ed60bd84c24a16a85f66939ce2
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-27 13:48:32 +0200

    add main to EncodedLines

--- week-03/day-01/src/EncodedLines.java
+++ week-03/day-01/src/EncodedLines.java
@@ -1,8 +1,11 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by zandraa on 3/27/2017.
  */
 public class EncodedLines {
   public static void main(String[] args) {
-    
+    Path inputPath = Paths.get("assets/encoded-lines.txt");
   }
 }
