54d970f1f7b02b97823f6823a20709c254f69d0e
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-29 10:07:07 +0200

    add SquareInTheCenter that draws a green square at the middle of the canvas

---
+++ week-03/day-03/src/SquareInTheCenter.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class SquareInTheCenter {
+
+  public static void mainDraw(Graphics graphics){
+    // draw a green 10x10 square to the canvas' center.
+
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(145, 145, 10, 10);
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
