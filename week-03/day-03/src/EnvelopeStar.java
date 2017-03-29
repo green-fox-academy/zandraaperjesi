7220e4d878182d6d1d059e846733ca76390c8dcd
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-29 13:37:05 +0200

    add EnvelopeStar rotated with and angle of 90 degrees

--- week-03/day-03/src/EnvelopeStar.java
+++ week-03/day-03/src/EnvelopeStar.java
@@ -4,27 +4,24 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class LinePlayQuarters {
+public class EnvelopeStar {
 
   public static final int LINES = 13;
 
   public static void mainDraw(Graphics graphics){
     // reproduce this:
-    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/line-play/r1.png]
-    for(int i = 0; i < LINES; i++) {
-      lineDrawer(150 / LINES * i, 0, 150, 150 / LINES * i, graphics);
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/envelope-star/r2.png]
+    for (int i = 0; i < LINES; i++) {
+      lineDrawer(150 / LINES * i, 150, 150, 150 - 150 / LINES * i, graphics);
       lineDrawer(150 / LINES * i, 150, 150, 150 / LINES * i + 150, graphics);
-      lineDrawer(150 / LINES * i + 150, 0, 300, 150 / LINES * i, graphics);
-      lineDrawer(150 / LINES * i + 150, 150, 300, 150 / LINES * i + 150, graphics);
+      lineDrawer(300 - 150 / LINES * i, 150, 150, 150 - 150 / LINES * i, graphics);
+      lineDrawer(300 - 150 / LINES * i, 150, 150, 150 / LINES * i + 150, graphics);
     }
   }
 
   public static void lineDrawer(int x1, int y1, int x2, int y2, Graphics g) {
-    g.setColor(Color.MAGENTA);
+    g.setColor(Color.BLUE);
     g.drawLine(x1, y1, x2, y2);
-    g.setColor(Color.GREEN);
-    g.drawLine(x1, y1 + 150, x2 - 150, y2);
-
   }
 
   //    Don't touch the code below
