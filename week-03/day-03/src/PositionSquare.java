a6eaf463290a42803000d2af55e7b8e7420130ee
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-29 10:19:14 +0200

    add PositionSquare that draws 3 rectangles on the canvas

---
+++ week-03/day-03/src/PositionSquare.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    squareDrawer(12, 34, graphics);
+    squareDrawer(42, 74, graphics);
+    squareDrawer(129, 134, graphics);
+  }
+
+  public static void squareDrawer(int x, int y, Graphics g) {
+    g.drawRect(x, y, 50, 50);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
