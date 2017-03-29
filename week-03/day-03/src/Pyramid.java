80ab4598879d02ae4deefee2b7cd21edacb6b6ce
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-29 15:19:14 +0200

    add Pyramid that draws a triangles in triangle shape with changable height

--- week-03/day-03/src/Pyramid.java
+++ week-03/day-03/src/Pyramid.java
@@ -7,30 +7,40 @@ import java.util.List;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Pyramid {
-  public static final int HEIGHT = 3;
+  public static final int HEIGHT = 23;
 
   public static void mainDraw(Graphics graphics){
     // reproduce this:
     // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/triangles/r5.png]
 
-
-
+    pointCounter(graphics);
   }
 
-  public static void lineDrawer() {
+  public static void pointCounter(Graphics g) {
     for (int i = 1; i < HEIGHT + 1; i++) {
       List<Point> toConnect = new ArrayList<>();
       int startPointX = 150 - 6 * i;
       int startPointY = 20 + 10 * i;
-      for (int j = 0; j < HEIGHT * 2 + 1; j++) {
+      for (int j = 0; j < i * 2 + 1; j++) {
         toConnect.add(new Point(startPointX, startPointY));
         startPointX += 6;
-        startPointY += 10 * (j % i * (-1));
+        if (j % 2 == 0) {
+          startPointY += -10;
+        }
+        else {
+          startPointY += 10;
+        }
       }
-
+      lineDrawer(toConnect, g);
     }
   }
-  
+
+  public static void lineDrawer(List<Point> points, Graphics g) {
+    for (int i = 0; i < points.size() - 1; i++) {
+      g.drawLine((int) points.get(i).getX(), (int) points.get(i).getY(), (int) points.get(i + 1).getX(), (int) points.get(i + 1).getY());
+    }
+    g.drawLine((int) points.get(0).getX(), (int) points.get(0).getY(), (int) points.get(points.size() - 1).getX(), (int) points.get(points.size() - 1).getY());
+  }
 
   //    Don't touch the code below
   public static void main(String[] args) {
