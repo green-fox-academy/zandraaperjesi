e8d988d311432c3384a0674a5d97e02586b7a315
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-29 10:43:54 +0200

    make RainbowBox fill the canvs with the colors of rainbow

--- week-03/day-03/src/RainbowBox.java
+++ week-03/day-03/src/RainbowBox.java
@@ -5,15 +5,25 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RainbowBox {
+  public static Color VIOLET = new Color(249, 192, 255);
+  public static Color INDIGO = new Color(75, 0, 130);
 
   public static void mainDraw(Graphics graphics){
     // create a square drawing function that takes 2 parameters:
     // the square size, and the fill color,
     // and draws a square of that size and color to the center of the canvas.
     // create a loop that fills the canvas with rainbow colored squares.
+
+    Color[] colors = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, INDIGO, VIOLET};
+
+    for (int i = 0; i < 7; i++) {
+      squareDrawer(300 - i * 300 / 7, colors[i], graphics);
+
+    }
   }
 
   public static void squareDrawer(int size, Color color, Graphics g) {
+    g.setColor(color);
     g.fillRect(150 - size / 2, 150 - size / 2, size, size);
   }
 
