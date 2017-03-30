4e8b5911f05b64a91b867f0bab9111e5f68e2387
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-30 12:17:11 +0200

    add FractalHexagon that frows a hexagon based fractal on canvas

--- week-03/day-04/src/FractalHexagon.java
+++ week-03/day-04/src/FractalHexagon.java
@@ -7,15 +7,21 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class FractalHexagon {
 
   public static void mainDraw(Graphics graphics){
-
-
+    hexagonPrinterRec(4, 300, 300, 200, graphics);
   }
 
-  public static void hexagonPrinter(double x, double y, double diameter,  Graphics g) {
+
+  public static void hexagonPrinterRec(int depth, double x, double y, double diameter, Graphics g) {
     Polygon p = new Polygon();
     for (int i = 0; i < 6; i++)
       p.addPoint((int) (x + diameter * Math.cos(i * 2 * Math.PI / 6)), (int) (y + diameter * Math.sin(i * 2 * Math.PI / 6)));
     g.drawPolygon(p);
+
+    if (depth > 0) {
+      hexagonPrinterRec(depth - 1, x - diameter / 4, y - diameter / 2.33, diameter / 2, g);
+      hexagonPrinterRec(depth - 1, x + diameter / 2, y, diameter / 2, g);
+      hexagonPrinterRec(depth - 1, x - diameter / 4, y + diameter / 2.33, diameter / 2, g);
+    }
   }
 
   //    Don't touch the code below
@@ -31,6 +37,7 @@ public class FractalHexagon {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
+      this.setBackground(Color.green);
       mainDraw(graphics);
 
     }
