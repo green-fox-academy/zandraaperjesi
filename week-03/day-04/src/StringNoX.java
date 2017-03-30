9e728af8d137acb38c6a08683c44552345250c91
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-30 10:55:02 +0200

    add StringNoX that removes x chars from a string recursively

--- week-03/day-04/src/StringNoX.java
+++ week-03/day-04/src/StringNoX.java
@@ -1,13 +1,13 @@
-public class StringX {
+public class StringNoX {
   public static void main(String[] args) {
-    System.out.println(xToY("axaxxaxax"));
+    System.out.println(xToY("axxext"));
   }
 
   public static String xToY(String withX) {
     StringBuilder myStringBuilder = new StringBuilder(withX);
     if (myStringBuilder.length() == 1) {
       if (myStringBuilder.charAt(0) == (char) 88 || myStringBuilder.charAt(0) == (char) 120) {
-        myStringBuilder.setCharAt(0,'y');
+        myStringBuilder.deleteCharAt(0);
         return myStringBuilder.toString();
       }
       else {
@@ -16,8 +16,16 @@ public class StringX {
     }
     else {
       if (myStringBuilder.charAt(0) == (char) 88 || myStringBuilder.charAt(0) == (char) 120) {
-        myStringBuilder.setCharAt(0,'y');
-        return myStringBuilder.charAt(0) + xToY(myStringBuilder.toString().substring(1));
+        myStringBuilder.deleteCharAt(0);
+        if (myStringBuilder.length() > 1 && (myStringBuilder.charAt(0) == (char) 88 || myStringBuilder.charAt(0) == (char) 120)) {
+          return xToY(myStringBuilder.toString().substring(1));
+        }
+        if (myStringBuilder.length() > 1) {
+          return myStringBuilder.charAt(0) + xToY(myStringBuilder.toString().substring(1));
+        }
+        else {
+          return myStringBuilder.toString();
+        }
       }
       else {
         return myStringBuilder.charAt(0) + xToY(myStringBuilder.toString().substring(1));
