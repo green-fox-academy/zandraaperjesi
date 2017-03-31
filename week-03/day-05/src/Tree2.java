3ee040805fe997a3edd2b3912f53ec60d121685d
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-03-31 12:21:21 +0200

    add linewidth to tree

--- week-03/day-05/src/Tree2.java
+++ week-03/day-05/src/Tree2.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.TimerTask;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Tree2 {
-
 
   public static void mainDraw(Graphics graphics){
     int[] colors = randomRGB();
@@ -14,6 +14,7 @@ public class Tree2 {
 
   public static void lineDrawer(Graphics g, int x1, int y1, double angle, int depth, double length, int red, int green, int blue) {
     if (depth > 0) {
+      Graphics2D gTD = (Graphics2D) g;
       int x2 = x1 + (int) (Math.sin(Math.toRadians(angle)) * length);
       int y2 = y1 - (int) (Math.cos(Math.toRadians(angle)) * length);
       int upx = Math.abs(x1 - x2);
@@ -32,11 +33,11 @@ public class Tree2 {
       }
 
       green += depth;
-      g.setColor(new Color(red, green, blue));
+      gTD.setColor(new Color(red, green, blue));
+      gTD.setStroke(new BasicStroke((float)length / 5));
 
-      g.drawLine(x1, y1, x2, y2);
-      g.drawLine(x1, y1, upx, upy);
-
+      gTD.drawLine(x1, y1, x2, y2);
+      gTD.drawLine(x1, y1, upx, upy);
 
       lineDrawer(g, x2, y2, angle + 30, depth - 1, length * .8, (red + 10) % 255, green, Math.abs(blue - 10) % 255);
       lineDrawer(g, x2, y2, angle - 30, depth - 1, length * .8, (red + 10) % 255, green, Math.abs(blue - 10) % 255);
@@ -51,7 +52,6 @@ public class Tree2 {
     rgb[1] = 1 + (int)(Math.random() * 100);
     return rgb;
   }
-
 
   //    Don't touch the code below
   public static void main(String[] args) {
