ef47303ed866cb15b792283076e1a1f1ed100b23
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-03 16:08:49 +0200

    add passout and wakeup methods to pirates

---
+++ week-04/day-01/pirates/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    Pirates p1 = new Pirates();
+    Pirates p2 = new Pirates();
+
+    p1.drinkSomeRum();
+    p1.howsItGoingMate();
+    p1.drinkSomeRum();
+    p1.drinkSomeRum();
+    p1.drinkSomeRum();
+    p1.drinkSomeRum();
+    p1.howsItGoingMate();
+    p1.howsItGoingMate();
+    p1.wakeUp();
+  }
+}
--- week-04/day-01/pirates/src/Pirates.java
+++ week-04/day-01/pirates/src/Pirates.java
@@ -1,10 +1,12 @@
 public class Pirates {
   public int intoxicated;
   public boolean alive;
+  public boolean passeOut;
 
   public Pirates() {
     intoxicated = 0;
     alive = true;
+    passeOut = false;
   }
 
   public void drinkSomeRum() {
@@ -17,16 +19,20 @@ public class Pirates {
   }
 
   public void howsItGoingMate() {
-    if(alive) {
+    if(alive && !passeOut) {
       if (intoxicated <= 4) {
         System.out.println("Pour me anudder!");
       } else {
         System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        passeOut = true;
         intoxicated = 0;
       }
     }
-    else {
+    else if (!alive){
       System.out.println("He's dead.");
+    }
+    else {
+      System.out.println("He's passed out.");
     }
   }
 
@@ -53,5 +59,26 @@ public class Pirates {
     else{
       System.out.println("You can's fight dead pirates.");
     }
+  }
+
+  public void hasParrot() {
+    if(alive) {
+      RanNum rand = new RanNum();
+      int n = rand.randomNumber();
+      if(n < 1) {
+        System.out.println("has parrot.");
+      }
+      else {
+        System.out.println("no parrot.");
+      }
+    }
+    else {
+      System.out.println("he's dead.");
+    }
+  }
+
+  public void wakeUp() {
+    System.out.println("Huh?");
+    passeOut = false;
   }
 }
