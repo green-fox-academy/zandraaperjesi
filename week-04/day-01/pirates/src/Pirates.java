5ca9e30dd163b3d4c8874a70401fb9b750757e2f
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-03 15:37:40 +0200

    add additional methods to pirates

--- week-04/day-01/pirates/src/Pirates.java
+++ week-04/day-01/pirates/src/Pirates.java
@@ -1,21 +1,40 @@
 public class Pirates {
   public int intoxicated;
+  public boolean alive;
 
   public Pirates() {
     intoxicated = 0;
+    alive = true;
   }
 
   public void drinkSomeRum() {
-    intoxicated += 1;
+    if(alive) {
+      intoxicated += 1;
+    }
+    else {
+      System.out.println("He's dead.");
+    }
   }
 
   public void howsItGoingMate() {
-    if(intoxicated <= 4) {
-      System.out.println("Pour me anudder!");
+    if(alive) {
+      if (intoxicated <= 4) {
+        System.out.println("Pour me anudder!");
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        intoxicated = 0;
+      }
     }
     else {
-      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
-      intoxicated = 0;
+      System.out.println("He's dead.");
     }
+  }
+
+  public void die() {
+    alive = false;
+  }
+
+  public void brawl() {
+    
   }
 }
---
+++ week-04/day-01/pirates/src/Random.java
@@ -0,0 +1,6 @@
+public class Random {
+  public int randomWithRange() {
+    int range = (3 - 1) + 1;
+    return (int)(Math.random() * range) + 1;
+  }
+}
