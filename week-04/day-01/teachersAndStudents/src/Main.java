6751898f7d8afb30cf87f479165f52da51437260
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-03 13:48:59 +0200

    add Teacher and Student classes that interact with each other when called

---
+++ week-04/day-01/teachersAndStudents/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Student student1 = new Student();
+    Teacher teacher1 = new Teacher();
+
+    student1.question(teacher1);
+    teacher1.teach(student1);
+  }
+}
---
+++ week-04/day-01/teachersAndStudents/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+  public void learn() {
+    System.out.println("I am learning.");
+  }
+
+  public void  question(Teacher teacher) {
+    teacher.answer();
+  }
+}
