4098852a05c440250c5ef7140fa5fdbd58546253
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-04 15:29:55 +0200

    add Aircraft methods

--- week-04/day-02/aircraftCarrier/src/Aircraft.java
+++ week-04/day-02/aircraftCarrier/src/Aircraft.java
@@ -12,4 +12,25 @@ public class Aircraft {
   public int fight() {
     return this.ammo * this.baseDamage;
   }
+
+  public int refill(int ammoIn) {
+    if (ammoIn < maxAmmo - ammo) {
+      ammo += ammoIn;
+      return 0;
+    }
+    else {
+      int defic = maxAmmo - ammo;
+      ammo = maxAmmo;
+      return ammoIn - defic;
+    }
+  }
+
+  public String getType() {
+    return this.getClass().toString();
+  }
+
+  public String getStatus() {
+    return "Type " + this.getClass() + ", Ammo: " + this.ammo + ", Base Damage: "
+            + this.baseDamage + ", All Damage: " + this.ammo * this.baseDamage;
+  }
 }
