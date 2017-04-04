61a3de89bd5106925c463039fad2e2fe16cb7bfa
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-04 13:21:11 +0200

    add contructors to Sponsor class

--- week-04/day-02/greenFoxOrg/src/Sponsor.java
+++ week-04/day-02/greenFoxOrg/src/Sponsor.java
@@ -2,6 +2,18 @@ public class Sponsor extends Person{
   private String company;
   private int hiredStudents;
 
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    super();
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
   @Override
   public void introduce() {
     System.out.println("Hi, I'm " + this.getName() + ", a " + getAge() +" year old " + getGender()
