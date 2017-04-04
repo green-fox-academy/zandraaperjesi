4e651cfde297910657a0b21d210f45a5daf522e6
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-04 11:42:47 +0200

    add Main class and add constructors to Person class

--- week-04/day-02/greenFoxOrg/src/Person.java
+++ week-04/day-02/greenFoxOrg/src/Person.java
@@ -3,16 +3,23 @@ public class Person {
   private int age;
   private String gender;
 
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
   public void introduce() {
     System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
   }
 
   public void getGoal() {
     System.out.println("My goal is: Live for the moment!");
-  }
-
-  public static void main(String[] args) {
-    Person p = new Person();
-    p.introduce();
   }
 }
