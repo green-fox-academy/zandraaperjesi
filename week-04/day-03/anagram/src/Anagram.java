c6959460a61434b0ed85818d9ff15277cb538c85
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-05 12:14:49 +0200

    add anagram class with false and true tests

--- week-04/day-03/anagram/src/Anagram.java
+++ week-04/day-03/anagram/src/Anagram.java
@@ -1,12 +1,21 @@
-public class Anagram {
-  public String inputString;
+import java.util.Arrays;
 
-  public Anagram(String inputString) {
-    this.inputString = inputString;
+public class Anagram {
+  private String stringOne;
+  private String stringTwo;
+
+  public Anagram(String stringOne, String stringTwo) {
+    this.stringOne = stringOne;
+    this.stringTwo = stringTwo;
   }
 
-  public boolean checkForAnagram(String toCheck) {
-    StringBuilder myStringbuilder = new StringBuilder(toCheck);
-    return toCheck.equals(myStringbuilder.reverse().toString());
+  public boolean isAnagram () {
+    char[] firstArray = this.stringOne.toCharArray();
+    char[] secondArray = this.stringTwo.toCharArray();
+    Arrays.sort(firstArray);
+    Arrays.sort(secondArray);
+    String firstWord = new String(firstArray);
+    String secondWord = new String(secondArray);
+    return firstWord.equals(secondWord);
   }
 }
--- week-04/day-03/anagram/test/AnagramTest.java
+++ week-04/day-03/anagram/test/AnagramTest.java
@@ -1,18 +1,19 @@
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnagramTest {
   public Anagram anagramDecider;
 
-  @Before
-  public void beforeTests() {
-
+  @Test
+  public void checkForAnagram() throws Exception {
+    anagramDecider = new Anagram("dog", "god");
+    assertTrue(anagramDecider.isAnagram());
   }
 
   @Test
-  public void checkForAnagram() throws Exception {
-    assertTrue();
+  public void checkForNotAnagram() throws Exception {
+    anagramDecider = new Anagram("dog", "pupper");
+    assertFalse(anagramDecider.isAnagram());
   }
 
 }
