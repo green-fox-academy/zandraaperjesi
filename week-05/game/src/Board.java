5aafd14b991b954edc1685efe83fedeaccbe9f6e
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-11 10:37:47 +0200

    add drawmap method to Board

---
+++ week-05/game/src/Board.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Board extends JComponent implements KeyListener {
+
+  int testBoxX;
+  int testBoxY;
+
+  public Board() {
+    testBoxX = 300;
+    testBoxY = 300;
+
+    // set the size of your draw board
+    setPreferredSize(new Dimension(720, 720));
+    setVisible(true);
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    super.paint(graphics);
+    graphics.fillRect(testBoxX, testBoxY, 100, 100);
+    // here you have a 720x720 canvas
+    // you can create and draw an image using the class below e.g.
+    PositionedImage image = new PositionedImage("wall.png", 300, 300);
+    drawmap(graphics);
+    image.draw(graphics);
+  }
+
+  public void drawmap(Graphics graphics) {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        PositionedImage image = new PositionedImage("floor.png", i * 72, j * 72);
+        image.draw(graphics);
+      }
+    }
+  }
+
+  // To be a KeyListener the class needs to have these 3 methods in it
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  // But actually we can use just this one for our goals here
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // When the up or down keys hit, we change the position of our box
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      testBoxY -= 100;
+    } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+      testBoxY += 100;
+    }
+    // and redraw to have a new picture with the new coordinates
+    repaint();
+  }
+}
