30398731b9d9b66c6eb73ac886bdc67b49dab91f
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-04-19 13:53:56 +0200

    add hashmap to main method in parkinglot

--- week-06/day-03/parkingLot/src/Main.java
+++ week-06/day-03/parkingLot/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
   public static void main(String[] args) {
+    List<Car> allCars = new ArrayList<>();
+    Map<String, Integer> carOccurs = new HashMap<>();
 
+    for (int i = 0; i < 256; i++) {
+      Car bufferCar = new Car(CarType.returnType(), CarColor.returnColor());
+      String bufferString = bufferCar.type.toString() + " " + bufferCar.color.toString();
+      if (carOccurs.containsKey(bufferString)) {
+        carOccurs.put(bufferString, carOccurs.get(bufferString) + 1);
+      }
+      else {
+        carOccurs.put(bufferString, 1);
+      }
+    }
   }
 }
