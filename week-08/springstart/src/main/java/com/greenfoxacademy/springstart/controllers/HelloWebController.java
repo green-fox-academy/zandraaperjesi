7614a578b0b3bc02b39255d8c26c5df551f914ab
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-02 12:29:10 +0200

    mod web app to greet and count the number of times the site had been reloadaed

--- week-08/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-08/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,12 +3,17 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  AtomicLong idCount = new AtomicLong();
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("idCount", idCount.addAndGet(1));
     return "greeting";
   }
 }
