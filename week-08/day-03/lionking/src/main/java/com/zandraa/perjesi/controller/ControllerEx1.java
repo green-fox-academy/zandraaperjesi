29c3d8319d6fb4a7d28035050fc418fc76b71678
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-03 14:17:41 +0200

    add exercise one

--- week-08/day-03/lionking/src/main/java/com/zandraa/perjesi/controller/ControllerEx1.java
+++ week-08/day-03/lionking/src/main/java/com/zandraa/perjesi/controller/ControllerEx1.java
@@ -1,10 +1,15 @@
 package com.zandraa.perjesi.controller;
-
 import com.zandraa.perjesi.model.BankAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerEx1 {
-
-  BankAccount simbaAcc = new BankAccount("Simba", 2000, "lion");
+  @RequestMapping("/exercise1")
+  public String exerciseOne(Model model) {
+    BankAccount simbaAcc = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("simbaAcc",simbaAcc);
+    return "index";
+  }
 }
