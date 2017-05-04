d8ec9f6d4ffbd81a997865f3fc31475a3832351a
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-04 12:09:41 +0200

    add exercise with updated intellij, so it actually works

---
+++ week-08/day-04/helloworldapp/src/main/java/com/zandraa/perjesi/HelloworldappApplication.java
@@ -0,0 +1,22 @@
+package com.zandraa.perjesi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldappApplication implements CommandLineRunner{
+  @Autowired
+  HelloWorld hw;
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloworldappApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    hw.setMessage("Hello, world!");
+    hw.getMessage();
+  }
+}
