270006c0de94b9dabc5d2a2d66986ccae72cd0ea
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-10 11:18:25 +0200

    add first exercise to rest controller exercise

---
+++ week-08/restbackend/src/main/java/com/zandraa/perjesi/model/Double.java
@@ -0,0 +1,7 @@
+package com.zandraa.perjesi.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Double {
+}
--- week-08/todoApp/src/main/java/com/zandraa/perjesi/repository/TodoRepository.java
+++ week-08/todoApp/src/main/java/com/zandraa/perjesi/repository/TodoRepository.java
@@ -2,6 +2,10 @@ package com.zandraa.perjesi.repository;
 
 import com.zandraa.perjesi.model.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
