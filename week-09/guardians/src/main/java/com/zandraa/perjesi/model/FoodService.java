afb08b86bc917122b67f75382af4d13299e06c98
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-05-15 17:20:45 +0200

    add methods to foodservice class

--- week-09/guardians/src/main/java/com/zandraa/perjesi/model/FoodService.java
+++ week-09/guardians/src/main/java/com/zandraa/perjesi/model/FoodService.java
@@ -15,4 +15,16 @@ public class FoodService {
   public List<Food> allFood() {
     return foodRepository.getAll();
   }
+
+  public List<Food> addFood(String name, int calorie, int amount) {
+    return foodRepository.addFood(new Food(name, calorie, amount));
+  }
+
+  public List<Food> deleteFood(String name) {
+    return foodRepository.deleteFood(name);
+  }
+  
+  public List<Food> changeAmount(String name, int amount) {
+    return foodRepository.changeAmount(name, amount);
+  }
 }
